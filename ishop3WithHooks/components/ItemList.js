db278e5f7f981821dcdce68c2f3cdb24048b208c
Dima-Mitrich <dima_mitrichenko@mail.ru>
2021-05-17 23:21:40 +0300

    first try

---
+++ ishop3WithHooks/components/ItemList.js
@@ -0,0 +1,178 @@
+"use strict";
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './ItemList.css'
+
+class ItemList extends React.Component {
+
+    static propTypes = {
+        mode: PropTypes.string.isRequired,
+        name: PropTypes.string,
+        url: PropTypes.string,
+        cbSaveChanges: PropTypes.func.isRequired,
+        cbItemWasCreated: PropTypes.func.isRequired,
+        cbItemWasChanged: PropTypes.func.isRequired,
+        cbCancelChanges: PropTypes.func.isRequired,
+    }
+
+    state = {
+        name: this.props.name,
+        price: this.props.price,
+        url: this.props.url,
+        remainder: this.props.remainder,
+        isNameIrrelevant: false,
+        isPriceIrrelevant: false,
+        isUrlIrrelevant: false,
+        isRemainderIrrelevant: false,
+        isEditing: false,
+    }
+
+    //при изменении товара меняем и стейт
+    componentDidUpdate(prevProps) {
+        if (prevProps.id !== this.props.id) this.setState({
+            name: this.props.name,
+            price: this.props.price,
+            url: this.props.url,
+            remainder: this.props.remainder,
+        })
+    }
+
+    //валидация имени
+    onChangeNameInput = (event) => {
+        let currValue = event.target.value;
+        if (!currValue || currValue.length > 15) this.setState(() => {
+            return (
+                { name: currValue, isNameIrrelevant: true, isEditing: true })
+        });
+        else this.setState(() => { return { name: currValue, isNameIrrelevant: false, isEditing: true } })
+        this.props.cbItemWasChanged();
+    }
+
+    //валидация цены
+    onChangePriceInput = (event) => {
+        let currValue = event.target.value;
+        if (isNaN(currValue) || !currValue) this.setState(() => {
+            return (
+                { price: currValue, isPriceIrrelevant: true, isEditing: true })
+        });
+        else this.setState(() => { return { price: currValue, isPriceIrrelevant: false, isEditing: true } });
+        this.props.cbItemWasChanged();
+    }
+
+    //валидация урл
+    onChangeUrlInput = (event) => {
+        let urlRegExp = /^[www.]+([a-zA-Z0-9]([a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,6}$/;
+        let currValue = event.target.value;
+        if (!urlRegExp.test(currValue) || !currValue) {
+            this.setState(() => {
+                return (
+                    { url: currValue, isUrlIrrelevant: true, isEditing: true })
+            });
+        }
+        else this.setState(() => { return { url: currValue, isUrlIrrelevant: false, isEditing: true } })
+        this.props.cbItemWasChanged();
+    }
+
+    //валидация остатка
+    onChangeRemainderInput = (event) => {
+        let currValue = event.target.value;
+        if (isNaN(currValue) || !currValue) this.setState(() => {
+            return (
+                { remainder: currValue, isRemainderIrrelevant: true, isEditing: true })
+        });
+        else this.setState(() => { return { remainder: currValue, isRemainderIrrelevant: false, isEditing: true } });
+        this.props.cbItemWasChanged();
+    }
+
+    saveChanges = () => {
+        let params = {
+            name: this.state.name,
+            price: parseFloat(this.state.price),
+            url: this.state.url,
+            remainder: parseFloat(this.state.remainder),
+            id: this.props.id,
+        }
+        this.setState({ isEditing: false });
+        this.props.cbSaveChanges(params);
+    }
+
+    saveNewItem = () => {
+        let params = {
+            name: this.state.name,
+            price: parseFloat(this.state.price),
+            url: this.state.url,
+            remainder: parseFloat(this.state.remainder),
+        }
+        this.props.cbItemWasCreated(params)
+    }
+
+    cancelChanges = () => {
+        this.setState({ isEditing: false });
+        this.props.cbCancelChanges();
+    }
+
+    render() {
+        if (this.props.mode == 'view') {
+            return (
+                <div>
+                    <span>
+                        <strong>{this.props.name}<br /></strong>
+                    </span>
+                    <span>{this.props.price + '$'}<br /></span>
+                    <a href={this.props.url}>{this.props.url}<br /></a>
+                    <span>{this.props.remainder + 'pairs'}<br /></span>
+                </div>
+            );
+        } else {
+            return (
+                <div>
+                    <span>Name:</span>
+                    <input type='text'
+                        value={this.state.name}
+                        onChange={this.onChangeNameInput} />
+                    {
+                        (this.state.isNameIrrelevant) && <span className='ErrorMessage'> Error Valid</span>
+                    }
+                    <br />
+
+                    <span>Price:</span>
+                    <input type='text'
+                        value={this.state.price}
+                        onChange={this.onChangePriceInput} />
+                    {
+                        (this.state.isPriceIrrelevant) && <span className='ErrorMessage'> Error Valid</span>
+                    }
+                    <br />
+
+                    <span>URL:</span>
+                    <input type='text'
+                        value={this.state.url}
+                        onChange={this.onChangeUrlInput} />
+                    {
+                        (this.state.isUrlIrrelevant) && <span className='ErrorMessage'> Error Valid</span>
+                    }
+                    <br />
+
+                    <span>Remainder:</span>
+                    <input type='text'
+                        value={this.state.remainder}
+                        onChange={this.onChangeRemainderInput} />
+                    {
+                        (this.state.isRemainderIrrelevant) && <span className='ErrorMessage'> Error Valid</span>
+                    }
+                    <br />
+
+                    <button onClick={this.props.mode == 'edit' ? this.saveChanges : this.saveNewItem}
+                        disabled={this.state.isNameIrrelevant || this.state.isPriceIrrelevant ||
+                            this.state.isRemainderIrrelevant || this.state.isUrlIrrelevant ||
+                            !this.state.isEditing}>Save</button>
+                    <button onClick={this.cancelChanges}>Cancel</button>
+                </div>
+            );
+        }
+    }
+}
+
+export default ItemList;
---
+++ ishop3WithHooks/components/MyStore.js
@@ -0,0 +1,128 @@
+import React, { Fragment } from 'react';
+import MyItem from './MyGoods';
+import PropTypes, { number } from 'prop-types';
+import ItemList from './ItemList'
+
+import './MyStore.css';
+
+class MyStore extends React.Component {
+
+    static propTypes = {
+        name: PropTypes.string.isRequired,
+        goods: PropTypes.array.isRequired,
+    }
+
+    state = {
+        selectedItem: null,
+        itemsList: this.props.goods,
+        mode: null,
+        itemIsEditing: false,
+    }
+
+    itemWasSelected = (id) => {
+        let item = this.state.itemsList.find(elem => elem.id == id);
+        this.setState(() => {
+            return {
+                selectedItem: item,
+                mode: 'view'
+            }
+        })
+    }
+
+    deleteItem = (id) => {
+        if (confirm('are you sure?')) {
+            let arr = this.state.itemsList.filter(elem => elem.id != id);
+            this.setState({ itemsList: arr });
+        }
+    }
+
+    editItem = (id) => {
+        let item = this.state.itemsList.find(elem => elem.id == id);
+        this.setState(() => {
+            return {
+                selectedItem: item,
+                mode: 'edit',
+            }
+        })
+    }
+
+    saveChanges = (params) => {
+        let item = this.state.itemsList.find(elem => elem.id == params.id);
+        item.name = params.name;
+        item.price = params.price;
+        item.photoURL = params.url;
+        item.remainder = params.remainder;
+        this.setState(() => { return { mode: null, itemIsEditing: false } });
+    }
+
+    cancelChanges = () => {
+        this.setState(() => { return { mode: null, itemIsEditing: false } })
+    }
+
+    itemWasChanged = () => {
+        this.setState({ itemIsEditing: true })
+    }
+
+    newItemCreate = () => {
+        this.setState({ mode: 'new', itemIsEditing: true, selectedItem: null })
+    }
+
+    itemWasCreated = (params) => {
+        let currentList = this.state.itemsList;
+        let newItem = {
+            name: params.name,
+            price: params.price,
+            photoURL: params.url,
+            remainder: params.remainder,
+            id: currentList[currentList.length - 1].id + 1
+        }
+        currentList.push(newItem);
+        this.setState({ itemsList: currentList, mode: null, itemIsEditing: false })
+    }
+
+    render() {
+
+        let arr = this.state.itemsList.map(element =>
+            <MyItem
+                name={element.name}
+                price={element.price}
+                url={element.photoURL}
+                remainder={element.remainder}
+                id={element.id}
+                key={element.id}
+                cbDeleteItem={this.deleteItem}
+                cbSelectItem={this.itemWasSelected}
+                cbEditItem={this.editItem}
+                editNow={this.state.itemIsEditing}
+                className={this.state.selectedItem && this.state.selectedItem.id == element.id ? 'selectedItem' : null}
+            />
+        )
+
+        return (
+            <Fragment>
+                <div>
+                    <span>{this.props.name}</span>
+                    <table className='MyStoreClass'>
+                        <tbody>{arr}</tbody>
+                    </table>
+                    <button type='button' onClick={this.newItemCreate}
+                        disabled={this.state.mode && this.state.mode != 'view' ? true : false}>New item</button>
+                </div>
+                {
+                    (this.state.mode) && <ItemList mode={this.state.mode}
+                        name={this.state.selectedItem ? this.state.selectedItem.name : ''}
+                        price={this.state.selectedItem ? this.state.selectedItem.price : ''}
+                        remainder={this.state.selectedItem ? this.state.selectedItem.remainder : ''}
+                        url={this.state.selectedItem ? this.state.selectedItem.photoURL : ''}
+                        id={this.state.selectedItem ? this.state.selectedItem.id : null}
+                        cbSaveChanges={this.saveChanges}
+                        cbCancelChanges={this.cancelChanges}
+                        cbItemWasChanged={this.itemWasChanged}
+                        cbItemWasCreated={this.itemWasCreated} />
+                }
+            </Fragment>
+        );
+    }
+}
+
+export default MyStore;
